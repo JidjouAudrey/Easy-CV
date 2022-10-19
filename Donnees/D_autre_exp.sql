687eda1b7ee92a95d136baa15278f7d79b06948c
BieleuRoussel <bieleuroussel@gmail.com>
2022-10-19 22:16:20 +0200

    Ajout des donnees

---
+++ Donnees/D_autre_exp.sql
@@ -0,0 +1,45 @@
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(1,'Musical','15/05/1997','26/11/2012',1);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(2,'Artistique','22/09/2000','25/10/2018',2);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(3,'Musical','23/05/2001','30/12/2015',3);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(4,'Commercial','10/05/1998','20/08/2020',4);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(5,'Musical','10/05/2004','29/11/2019',5);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(6,'Artistique','12/05/1997','28/11/2012',6);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(7,'Musical','08/05/1998','26/09/2019',7);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(8,'Commercial','15/10/2000','26/12/2016',8);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(9,'Marketing','18/05/2000','20/08/2012',9);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(10,'Commercial','18/10/2000','10/12/2015',10);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(11,'Artistique','15/11/2002','26/12/2017',11);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(12,'Ingenieure','19/11/2005','10/02/2019',12);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(13,'Marketing','19/05/2002','11/05/2019',13);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(14,'Programmeur','08/10/1998','12/08/2019',14);
+
+INSERT INTO (id_AutExp, nom_Aut, dateDeb_Aut, dateFin_Aut, id_experience)
+VALUES(15,'Ingenieure','17/05/2000','10/11/2015',15);
+
