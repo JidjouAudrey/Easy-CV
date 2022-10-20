fd7b15b76c740499979b6ba45fdf71adc7fe56f8
Lalou <115021998+TankoLeila@users.noreply.github.com>
2022-10-20 17:46:29 +0100

    Ajout des donnees

---
+++ Donnees/D_Competences.sql
@@ -0,0 +1,45 @@
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (1,"Conducteur de camion",1);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (2,"Developpeur Front-end",2);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (3,"Developpeur Back-end",3);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (4,"Developpeur Full stack",4);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (5,"Developpeur Back-end",5);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (6,"Infographe",6);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (7,"Infographe",7);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (8,"Developpeur Full stack",8);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (9,"Developpeur Full stack",9);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (10,"Administrateur de base de donnees",10);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (11,"Data analyste ",11);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (12,"Administrateur de base de donnees",12);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (13,"Data analyste",13);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (14,"Architecte Reseau",14);
+
+INSERT INTO Competences (id_competence, nom_competence, id_profil)
+VALUES (15,"Consultant de systeme d'information",15);
+
---
+++ Donnees/D_Projet.sql
@@ -0,0 +1,44 @@
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (1,"Gestion de Curriculum Vitae",'07/04/2021','15/09/2022',1);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (2,"Application de localisation universitaire",'17/05/2020','01/06/2021',2);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (3,"Gestion de restaurant",'01/02/2019','15/01/2020',3);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (4,"Gestion de parking",'01/01/2018','16/11/2018',4);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (5,"Application de location de maisons",'14/02/2020','13/05/2021',5);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (6,"Application de petits metiers",'06/02/1995','05/09/1995',6);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (7,"Gestion stock",'31/04/1991','30/05/1991',7);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (8,"Gestion de Curriculum Vitae",'01/03/2000','15/03/2001',8);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (9,"Gestion de Tontine",'07/04/2021','15/09/2022',9);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (10,"Gestion d'information d'entreprise",'10/10/2021','05/11/2022',10);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (11,"Application d'Alimentation",'12/04/1999','12/04/2000',11);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (12,"Gestion de Curriculum Vitae",'17/04/1996','17/04/1997',12);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (13,"Application de reservation d'hotel",'18/03/2000','18/03/2001',13);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (14,"Gestion de tontine",'10/05/1973','01/09/1994',14);
+
+INSERT INTO Projets (id_projet, nom_projet, dateDeb_projet, dateFin_projet, id_experience)
+VALUES (15,"Gestion de Tontine",'07/04/2021','15/09/2022',15);
