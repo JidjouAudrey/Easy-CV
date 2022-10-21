321a89752c65b49e4e85983d7ee1324963a0dc78
jidjou audrey <audrey.djampou@gmail.com>
2022-10-21 19:38:05 +0100

    Ajout des fonctionnalites

---
+++ Fonctionnalites/F_Profiles/add_profiles.sql
@@ -0,0 +1,4 @@
+INSERT INTO Profiles (id_profil,nom,prenom ,adresse ,date_naiss,statut_social ,id_utilisateur) 
+VALUES (&id_profil,'&nom','&prenom' ,'&adresse' ,'&date_naiss','&statut_social' ,&id_utilisateur);
+
+
